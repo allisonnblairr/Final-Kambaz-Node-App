5bca4f86dfcfe89ddf4d98ec69f4e5935c271925
Anzhuo-W <45926251+Anzhuo-W@users.noreply.github.com>
2025-04-24 15:15:50 -0400

    delete old possibleAnswers

--- Kambaz/GivenAnswers/dao.js
+++ Kambaz/GivenAnswers/dao.js
@@ -1,16 +1,19 @@
 import model from "./model.js";
-import { v4 as uuidv4 } from "uuid";
+import {v4 as uuidv4} from "uuid";
 
 export function findGivenAnswersForQuestion(questionId) {
-    return model.find({ question: questionId });
+    return model.find({questionId: questionId});
 }
+
 export function createGivenAnswer(givenAnswer) {
-    const newGivenAnswer = { ...givenAnswer, _id: uuidv4() };
+    const newGivenAnswer = {...givenAnswer, _id: uuidv4()};
     return model.create(newGivenAnswer);
 }
+
 export function deleteGivenAnswer(givenAnswerId) {
-    return model.deleteOne({ _id: givenAnswerId });
+    return model.deleteOne({_id: givenAnswerId});
 }
+
 export function updateGivenAnswer(givenAnswerId, givenAnswerUpdates) {
-    return model.updateOne({ _id: givenAnswerId }, givenAnswerUpdates);
+    return model.updateOne({_id: givenAnswerId}, givenAnswerUpdates);
 }
--- Kambaz/PossibleAnswers/dao.js
+++ Kambaz/PossibleAnswers/dao.js
@@ -1,19 +1,23 @@
 import model from "./model.js";
-import { v4 as uuidv4 } from "uuid";
+import {v4 as uuidv4} from "uuid";
 
 export function findPossibleAnswersForQuestion(questionId) {
-    return model.find({ question: questionId });
+    return model.find({questionId: questionId});
 }
+
 export function findPossibleAnswerForQuestionById(questionId, paId) {
-    return model.findOne({ question: questionId, _id: paId });
+    return model.findOne({questionId: questionId, _id: paId});
 }
+
 export function createPossibleAnswer(possibleAnswer) {
-    const newPossibleAnswer = { ...possibleAnswer, _id: uuidv4() };
+    const newPossibleAnswer = {...possibleAnswer, _id: uuidv4()};
     return model.create(newPossibleAnswer);
 }
+
 export function deletePossibleAnswer(possibleAnswerId) {
-    return model.deleteOne({ _id: possibleAnswerId });
+    return model.deleteOne({_id: possibleAnswerId});
 }
+
 export function updatePossibleAnswer(possibleAnswerId, possibleAnswerUpdates) {
-    return model.updateOne({ _id: possibleAnswerId }, possibleAnswerUpdates);
+    return model.updateOne({_id: possibleAnswerId}, possibleAnswerUpdates);
 }
